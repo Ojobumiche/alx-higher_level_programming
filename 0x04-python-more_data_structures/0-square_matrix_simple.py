2047e9ed33f585f1b3dc55aba4507e72e0ec27ce
Ojobumiche <bumiche@gmail.com>
2023-03-15 19:34:02 -0700

    compute matrice

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    result = [[0 for j in range(len(matrix[i]))] for i in range(len(matrix))]
-    for i in range(len(matrix)):
-        for j in range(len(matrix[i])):
-            result[i][j] = matrix[i][j] ** 2
-    return result
+    new_matrix = [[x ** 2 for x in row] for row in matrix]
+    return new_matrix
