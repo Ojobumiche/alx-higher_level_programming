d2d15bc2e3105752227c15d922599eca4c9e6d5f
Ojobumiche <bumiche@gmail.com>
2023-04-12 09:19:55 -0700

    BeseGeometry_1

---
+++ 0x0A-python-inheritance/tests/6-mian.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+BaseGeometry = __import__('6-base_geometry').BaseGeomery
+
+
+
+
+bg = BaseGeometry()
+
+try:
+    print(bg.area())
+except Exception as e:
+    print("[{}] {}".format(e.__class__.__name__, e))
