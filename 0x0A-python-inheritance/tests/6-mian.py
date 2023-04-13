2456da2c00612275dd673e89fb5c4ea0262ee162
Ojobumiche <bumiche@gmail.com>
2023-04-13 04:52:48 -0700

    baseGeometry

--- 0x0A-python-inheritance/tests/6-mian.py
+++ 0x0A-python-inheritance/tests/6-mian.py
@@ -2,13 +2,10 @@
 BaseGeometry = __import__('6-base_geometry').BaseGeomery
 
 
-
-
 bg = BaseGeometry()
 
 try:
     print(bg.area())
 
 except Exception as e:
-
     print("[{}] {}".format(e.__class__.__name__, e))
