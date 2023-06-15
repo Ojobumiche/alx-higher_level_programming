bb86d5bb7cbc1b5185a08e8c975a776ec3e43003
Ojobumiche <bumiche@gmail.com>
2023-06-15 12:43:31 -0700

    value of my argument

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
-const [arg] = process.argv.slice(2);
-if (arg) {
-  console.log(arg);
+const args = process.argv.slice(2);
+if (args[0]) {
+  console.log(args[0]);
 } else {
-  console.log("No argument");
+  console.log('No argument');
 }
