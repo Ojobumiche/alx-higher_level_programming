829b19cb7c975079cf43dcb6b538c12946a892ab
Ojobumiche <bumiche@gmail.com>
2023-06-15 13:40:46 -0700

    Factorial

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -3,10 +3,11 @@ function factorial (num) {
   if (isNaN(num)) {
     return 1;
   }
-  if (num === 0 || num === 1) {
+  if (num === 0) {
     return 1;
+  } else {
+    return num * factorial(num - 1);
   }
-  return num * factorial(num - 1);
 }
 const input = parseInt(process.argv[2]);
 const result = factorial(input);
