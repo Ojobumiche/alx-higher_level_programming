234ec95d5148b78ac336ed4317c1604c4c22458d
Ojobumiche <bumiche@gmail.com>
2023-06-14 17:10:13 -0700

    An Integer

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,7 +1,3 @@
 #!/usr/bin/node
-const num = parseInt(arg);
-if (!isNaN(num)) {
-	  console.log(`My number: ${num}`);
-} else {
-	  console.log('Not a number');
-}
+const num = Math.floor(Number(process.argv[2]));
+console.log(isNaN(num) ? 'Not a number' : `My number: ${num}`);
