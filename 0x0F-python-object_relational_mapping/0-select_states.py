ab569e3a906ed8009fa433b9710efb61215cf958
Ojobumiche <bumiche@gmail.com>
2023-06-19 06:37:21 -0700

    Get all states

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,25 +1,27 @@
 #!/usr/bin/python3
-import MySQLdb
 
-def list_state(user_name, password, database):
-    db = MySQLdb.connect(host='localhost', port=3306, user='user_name', 
-            password='password', db='database');
-    cursor = db.cursor()
-    cursor.execute("SELECT * FROM states ORDER BY id ASC");
-    state = cursor.fetchall();
+""" script that display all states from the database hbtn_0e_0_usa: """
+
+import MySQLdb
+import sys
+
+
+def list_states():
+    db_connect = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3]
+        )
+    db_cursor = db_connect.cursor()
+    db_cursor.execute("SELECT * FROM states ORDER BY id ASC")
+    states = db_cursor.fetchall()
     for state in states:
         print(state)
-    
-    cursor.close()
-    db.close()
+    db_cursor.close()
+    db_connect.close()
 
-    if __name__ == '__main__':
-        import sys
-        if len(sys.argv) < 4:
-            print("Usage: python list_states.py <user_name> <password> <database>")
-        else: 
-            user_name = sys.argv[1]
-            password  = sys.argv[2]
-            database = sys.argv[3]
-            list_states(user_name, password, database)
 
+if __name__ == "__main__":
+    list_states()
