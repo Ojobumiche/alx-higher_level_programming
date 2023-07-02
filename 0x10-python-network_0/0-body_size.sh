2b81de45a9ef11bedc3ebcbcf538753be5d156af
Ojobumiche <bumiche@gmail.com>
2023-07-02 10:44:00 +0000

    curl body size

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Get the comtent-lenght of a giving ip address
-curl -sI "$1" | awk '/Content-Length/{print $2}'
+# Get the content-length of a given IP address
+curl -sI "$1" | awk -F ': ' '/Content-Length/ {print $2}'
