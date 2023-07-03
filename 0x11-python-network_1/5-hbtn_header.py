990affa554d13cae06325b8b5f9d4488cf638827
Ojobumiche <bumiche@gmail.com>
2023-07-03 10:34:38 -0700

    response header value#1

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,12 +1,11 @@
-#!/usr/bin/python3 
- """ a script that takes in a URL, sends a request to same and displays 
- the value of the variable X-Request-Id in the response header""" 
- import sys 
- import requests 
-  
-  
- if __name__ == "__main__": 
-     url = sys.argv[1] 
-  
-     req = requests.get(url) 
-     print(req.headers.get("X-Request-Id"))
+#!/usr/bin/python3
+"""Python script that displays the response header value"""
+import sys
+import requests
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+
+    r = requests.get(url)
+    print(r.headers.get("X-Request-Id"))
+
