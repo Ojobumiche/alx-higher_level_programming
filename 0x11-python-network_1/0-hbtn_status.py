6fc0cba4d583e47e8fc16c827b6cb3b379046f7f
Ojobumiche <bumiche@gmail.com>
2023-07-06 08:05:07 -0700

    what is my status?

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
-"""Python script that fetches URL."""
+"""a script that getches url"""
 import urllib.request
 
 url = "https://alx-intranet.hbtn.io/status"
-with urllib.request.urlopen(request) as response:
+
+with urllib.request.urlopen(url) as response:
     body = response.read()
+
 print("Body response:")
-print("\t- type: {}".format(type(body)))
-print("\t- content: {}".format(body))
-print("\t- utf8 content: {}".format(body.decode("utf-8")))
+print("\t- type:", type(body))
+print("\t- content:", body)
+print("\t- utf8 content:", body.decode("utf-8"))
