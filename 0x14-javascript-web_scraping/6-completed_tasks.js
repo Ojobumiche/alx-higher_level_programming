dffdac9d769216ef7d4b73db034bdcfd009061b3
Ojobumiche <bumiche@gmail.com>
2023-07-26 12:06:35 +0000

    how many completed

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,19 +1,23 @@
 #!/usr/bin/node
 const request = require('request');
+
 request(process.argv[2], function (error, response, body) {
   if (!error && response.statusCode === 200) {
-    const todos = JSON.parse(body);
-    let completedTask = {};
-    todos.forEach((todo) => {
-      if (todo.completed) {
-        if (completedTask[todo.userId] === undefined) {
-          completedTask[todo.userId] = 1;
-        } else {
-          completedTask[todo.userId] += 1;
+    try {
+      const todos = JSON.parse(body);
+      let completedTask = {};
+      todos.forEach((todo) => {
+        if (todo.completed) {
+          if (completedTask[todo.userId] === undefined) {            completedTask[todo.userId] = 1;
+          } else {
+            completedTask[todo.userId] += 1;
+          }
         }
-      }
-    });
-    console.log(completedTask);
+      });
+      console.log(completedTask);
+    } catch (e) {
+      console.error('Error: Unable to parse data from the API response as JSON.');
+    }
   } else {
     console.error('Error: Unable to fetch data from the API.');
   }
